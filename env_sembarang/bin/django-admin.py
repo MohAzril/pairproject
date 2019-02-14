eab214d2c1d7aee064fd11c3d866a09359381677
syahadianyusril <syahadianyusril@gmail.com>
2019-02-14 14:48:08 +0700

    remove env

--- env_sembarang/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/alphatech/Documents/Challanges/pairproject/env_sembarang/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
