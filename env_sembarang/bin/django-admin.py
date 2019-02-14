45321afc6b0b2ad1d964950c888596fb30fe93bc
MohAzril <azril@alphatech.id>
2019-02-14 14:25:30 +0700

    awal

---
+++ env_sembarang/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/alphatech/kumpulan/Pair_project_django/env_sembarang/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
