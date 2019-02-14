9cf90cd4e2eb7a5eea5fd23bc16920a8f31ef275
MohAzril <azril@alphatech.id>
2019-02-15 00:44:00 +0700

    bismillah fusion

--- project_foursquare/home/urls.py
+++ project_foursquare/home/urls.py
@@ -4,7 +4,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('park', views.home, name='home'),
-    path('park/<int:blog_id>', views.blog_detail, name='blog_detail'),
+    # path('park', views.park, name='park'),
+    path('park/<int:park_id>', views.park_detail, name='park_detail'),
+    path('park', views.input_post, name="input_post"),
     path('', views.mall, name='mall'),
 ]
