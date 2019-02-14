5c2067fc23e9fa3af7b60943aaa71512196681b9
syahadianyusril <syahadianyusril@gmail.com>
2019-02-14 16:41:46 +0700

    fix urls

--- project_foursquare/home/views.py
+++ project_foursquare/home/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 
 # Create your views here.
-ender(request, 'home/base.html')
-=======
+
 def home(request):
     return render(request, 'home/content.html',{})
 
