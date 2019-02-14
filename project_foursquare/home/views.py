d784f885b22b96b50e2299758744fc9a04bea5c0
syahadianyusril <syahadianyusril@gmail.com>
2019-02-14 17:10:54 +0700

    nav ganti biru

--- project_foursquare/home/views.py
+++ project_foursquare/home/views.py
@@ -2,8 +2,6 @@ from django.shortcuts import render, redirect
 from django.utils import timezone
 
 # Create your views here.
-ender(request, 'home/base.html')
-=======
 def home(request):
-    return render(request, 'home/content.html',{})
+    return render(request, 'home/base.html',{})
 
